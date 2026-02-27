f4f78f4128f64bfbd12159ed3806824716416091
hyyyyyyz <1783866380@qq.com>
2026-02-27 11:24:58 +0800

    chore:Add rsl_rl library for robots rl training

---
+++ rsl_rl/rsl_rl/storage/__init__.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2021-2026, ETH Zurich and NVIDIA CORPORATION
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""Transitions storage for the learning algorithm."""
+
+from .rollout_storage import RolloutStorage
+
+__all__ = ["RolloutStorage"]
